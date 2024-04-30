d439e89dbc75eeaf48cb16add73c97ead22461d7
Thomas Garcia <thomasakulla@gmail.com>
2024-04-30 13:48:33 -0400

    set up for compute() ready

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -1,12 +1,12 @@
 def compute(ip_address, res):
-
+	data_for_csv = [] #13 pieces of data can be placed here
 	"13 items are computed/returned here"
 
-	# return 13 items that are computed here OR BETTER YET PASS IT INTO RESULTS - so no need to return
+	# results(data_for_csv)
 
 def results(compute_data):
 	"""
-	Compile results into csv file
+	Compile results into csv file via unpacking variables from compute()
 	"""
 	# unpack list of items returned = compute_data
 	filename="project2_output.csv"
